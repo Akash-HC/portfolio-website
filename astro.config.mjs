2a3002dd5bbba3f845b983b89dcc95ab00ab128f
Akash-HC <akashchalkodu@gmail.com>
2026-06-27 04:02:55 -0300

    pushing changes related to the configs for vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://Akash-HC.github.io',
-  base: '/portfolio-website/', // Replace with your repository name
+  base: "/", // Replace with your repository name
   vite: {
     plugins: [tailwindcss()],
     server: {
