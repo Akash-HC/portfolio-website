50c8584d265151345ba69955273e05d79162874b
Akash-HC <akashchalkodu@gmail.com>
2026-06-27 03:39:16 -0300

    updted the astro.config.mjs to target the github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://Akash-HC.github.io',
+  base: '/portfolio-website/', // Replace with your repository name
   vite: {
     plugins: [tailwindcss()],
     server: {
